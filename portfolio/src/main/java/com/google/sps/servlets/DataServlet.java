89335d23186ebd8188cbab571104f2d49aaacdf9
Heidi Johnson <2heidij@gmail.com>
2020-06-09 20:21:19 +0000

    remove unneeded imports

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,8 +25,6 @@ import java.util.Map;
 import java.util.Set; 
 import java.util.AbstractMap;
 import com.google.gson.Gson;
-import java.lang.reflect.Type;
-import com.google.gson.reflect.TypeToken;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
